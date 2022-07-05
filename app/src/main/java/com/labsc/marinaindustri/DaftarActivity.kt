b80a274fee89bf1b89ea5b4f06762ebcef6e4972
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-05 17:23:13 +0700

    done

--- app/src/main/java/com/labsc/marinaindustri/DaftarActivity.kt
+++ app/src/main/java/com/labsc/marinaindustri/DaftarActivity.kt
@@ -3,9 +3,9 @@ package com.labsc.marinaindustri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Masuk : AppCompatActivity() {
+class DaftarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_masuk)
+        setContentView(R.layout.activity_daftar)
     }
 }
--- app/src/main/java/com/labsc/marinaindustri/MainActivity.kt
+++ app/src/main/java/com/labsc/marinaindustri/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.labsc.marinaindustri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splashscreen.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splashscreen)
+        setContentView(R.layout.activity_splashscreen)
+
+        splshMarina.alpha = 0f
+        splshMarina.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MasukActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+        splshIndustri.alpha = 0f
+        splshIndustri.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MasukActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
