dbc415be96b9fd049ef7ac9e068fdb8bc5b1eba9
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-06 17:10:09 +0700

    done

---
+++ app/src/main/java/com/labsc/marinaindustri/DaftarSuksesActivity.kt
@@ -0,0 +1,11 @@
+package com.labsc.marinaindustri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaftarSuksesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_sukses)
+    }
+}
