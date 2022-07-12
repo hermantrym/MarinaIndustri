485395aa29fd8e1e517e4c9e26f8997dde784375
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-12 17:11:27 +0700

    done

---
+++ app/src/main/java/com/labsc/marinaindustri/CheckoutBerhasilActivity.kt
@@ -0,0 +1,11 @@
+package com.labsc.marinaindustri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutBerhasilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout_berhasil)
+    }
+}
