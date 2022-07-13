efa99bae9a1622f96a7ce3f67b59a74f61800ad9
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-13 17:09:22 +0700

    done

---
+++ app/src/main/java/com/labsc/marinaindustri/NotifikasiDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.labsc.marinaindustri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotifikasiDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifikasi_detail)
+    }
+}
