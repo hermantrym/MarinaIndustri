c1dbff13431c8c959f89b6d9dacbad17144ae3e2
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-14 17:29:07 +0700

    done

---
+++ app/src/main/java/com/labsc/marinaindustri/ProfilEditActivity.kt
@@ -0,0 +1,11 @@
+package com.labsc.marinaindustri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_edit)
+    }
+}
