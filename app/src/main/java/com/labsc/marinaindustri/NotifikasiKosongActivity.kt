3ea5f17f148d93bf5bf5859ddd3021e239023a7e
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-14 14:40:34 +0700

    done

---
+++ app/src/main/java/com/labsc/marinaindustri/NotifikasiKosongActivity.kt
@@ -0,0 +1,11 @@
+package com.labsc.marinaindustri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotifikasiKosongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifikasi_kosong)
+    }
+}
