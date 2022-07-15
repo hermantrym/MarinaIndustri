26f6b81a7e1e862383e043b0015d8fa5306d432f
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-15 14:02:33 +0700

    done

--- app/src/main/java/com/labsc/marinaindustri/MainActivity.kt
+++ app/src/main/java/com/labsc/marinaindustri/MainActivity.kt
@@ -3,23 +3,23 @@ package com.labsc.marinaindustri
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_splashscreen.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_main)
 
-        splshMarina.alpha = 0f
-        splshMarina.animate().setDuration(1500).alpha(1f).withEndAction {
+        SplMarina.alpha = 0f
+        SplMarina.animate().setDuration(1500).alpha(1f).withEndAction {
             val i = Intent(this, MasukActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
 
-        splshIndustri.alpha = 0f
-        splshIndustri.animate().setDuration(1500).alpha(1f).withEndAction {
+        SplIndustri.alpha = 0f
+        SplIndustri.animate().setDuration(1500).alpha(1f).withEndAction {
             val i = Intent(this, MasukActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
