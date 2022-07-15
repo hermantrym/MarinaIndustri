b70c76127123059dc0965c95be6f2ec00147ca62
hermantrym <111201811499@mhs.dinus.ac.id>
2022-07-15 14:54:29 +0700

    done

--- app/src/main/java/com/labsc/marinaindustri/DaftarActivity.kt
+++ app/src/main/java/com/labsc/marinaindustri/DaftarActivity.kt
@@ -1,11 +1,33 @@
 package com.labsc.marinaindustri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_daftar.*
 
 class DaftarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daftar)
+
+        BtnMasuk.setOnClickListener {
+            val i = Intent(this, MasukActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
+        BtnDaftar.setOnClickListener {
+            val i = Intent(this, DaftarSuksesActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+        TextDaftar.setOnClickListener {
+            val i = Intent(this, DaftarSuksesActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/labsc/marinaindustri/MasukActivity.kt
+++ app/src/main/java/com/labsc/marinaindustri/MasukActivity.kt
@@ -1,11 +1,41 @@
 package com.labsc.marinaindustri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_masuk.*
+
 
 class MasukActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_masuk)
+
+        BtnDaftar.setOnClickListener {
+            val i = Intent(this, DaftarActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
+        BtnMasuk.setOnClickListener {
+            val i = Intent(this, BerandaActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+        TextMasuk.setOnClickListener {
+            val i = Intent(this, BerandaActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+        Lewati.setOnClickListener {
+            val i = Intent(this, BerandaActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
